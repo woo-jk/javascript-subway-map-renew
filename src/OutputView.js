51f93807472c0e02293cceb6b9ce8e46c1935ab6
woo-jk <201702042@o.cnu.ac.kr>
2022-12-16 20:21:46 +0900

    feat: OutputView 역 관련 출력 함수들 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,16 @@
-const OutputView = {};
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const OutputView = {
+  printRegisterStation() {
+    MissionUtils.Console.print("\n[INFO] 지하철 역이 등록되었습니다.");
+  },
+  printdeleteStation() {
+    MissionUtils.Console.print("\n[INFO] 지하철 역이 삭제되었습니다.");
+  },
+  printStations(stations) {
+    MissionUtils.Console.print("\n## 역 목록");
+    stations.forEach((station) => MissionUtils.Console.print(`[INFO] ${station.getName()}`));
+  },
+};
 
 module.exports = OutputView;
