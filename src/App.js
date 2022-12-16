b109eae3b7b35e1ca5c005d5718e58147160f2e1
woo-jk <201702042@o.cnu.ac.kr>
2022-12-16 21:06:47 +0900

    feat: App 역, 노선 초기 설정 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const Line = require("./domain/Line");
+const Station = require("./domain/Station");
 const Validator = require("./Validator");
 const InputView = require("./view/InputView");
 const OutputView = require("./view/OutputView");
@@ -9,6 +11,14 @@ class App {
   constructor() {
     this.#lineRepository = new LineRepository();
     this.#stationRepository = new StationRepository();
+  }
+
+  initSetting() {
+    ["2호선", "3호선", "신분당선"].forEach((name) => this.#lineRepository.addLine(new Line(name)));
+
+    ["교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"].forEach((name) =>
+      this.#stationRepository.addStation(new Station(anme))
+    );
   }
 
   play() {
