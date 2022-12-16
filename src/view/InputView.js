2cb98f2ec88037316ccca384e182b043a8ad64e7
woo-jk <201702042@o.cnu.ac.kr>
2022-12-16 23:08:02 +0900

    fix: InputView 기능 목록출력을 OutputView 로 옮김

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,18 +1,8 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const InputView = {
-  readMainFeature(callback) {
-    MissionUtils.Console.readLine(
-      "## 메인 화면\n1. 역 관리\n2. 노선 관리\n3.구간 관리 \n4. 지하철 노선도 출력\nQ. 종료\n\n## 원하는 기능을 선택하세요.\n",
-      callback
-    );
-  },
-
-  readStationFeature(callback) {
-    MissionUtils.Console.readLine(
-      "## 역 관리 화면\n1. 역 등록\n2. 역 삭제\n3. 역 조회\nB. 돌아가기\n\n## 원하는 기능을 선택하세요.\n",
-      callback
-    );
+  readFeature(callback) {
+    MissionUtils.Console.readLine("\n## 원하는 기능을 선택하세요.\n", callback);
   },
 
   readRegisterStation(callback) {
@@ -21,13 +11,6 @@ const InputView = {
 
   readDeleteStation(callback) {
     MissionUtils.Console.readLine("## 삭제할 역 이름을 입력하세요", callback);
-  },
-
-  readLineFeature(callback) {
-    MissionUtils.Console.readLine(
-      "## 노선 관리 화면\n1. 노선 등록\n2. 노선 삭제\n3. 노선 조회\nB. 돌아가기\n\n## 원하는 기능을 선택하세요.\n",
-      callback
-    );
   },
 
   readRegisterLine(callback) {
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,6 +1,24 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const OutputView = {
+  printMainFeature() {
+    MissionUtils.Console.print(
+      "\n## 메인 화면\n1. 역 관리\n2. 노선 관리\n3. 구간 관리 \n4. 지하철 노선도 출력\nQ. 종료\n"
+    );
+  },
+
+  printStationFeature() {
+    MissionUtils.Console.print("\n## 역 관리 화면\n1. 역 등록\n2. 역 삭제\n3. 역 조회\nB. 돌아가기\n");
+  },
+
+  printLineFeature() {
+    MissionUtils.Console.print("\n## 노선 관리 화면\n1. 노선 등록\n2. 노선 삭제\n3. 노선 조회\nB. 돌아가기\n");
+  },
+
+  printRouteFeature() {
+    MissionUtils.Console.print("## 구간 관리 화면\n1. 구간 등록\n2. 구간 삭제\nB. 돌아가기\n");
+  },
+
   printRegisterStation() {
     MissionUtils.Console.print("\n[INFO] 지하철 역이 등록되었습니다.");
   },
