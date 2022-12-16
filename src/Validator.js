f4d187b2e2e9e7facf24de22d3e6259f3959be04
woo-jk <201702042@o.cnu.ac.kr>
2022-12-16 23:39:26 +0900

    feat: Validator 역 관리 화면 입력 검증 함수 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,7 +1,13 @@
 const Validator = {
   valdateMainCommand(command) {
     if (command !== "1" && command !== "2" && command !== "3" && command !== "4" && command !== "Q") {
-      throw new Error("[ERROR] 1, 2, 3, 4, Q 중에 하나를 입력해야 합니다.");
+      throw new Error("[ERROR] 선택할 수 없는 기능입니다.");
+    }
+  },
+
+  validateStationCommand(command) {
+    if (command !== "1" && command !== "2" && command !== "3" && command !== "B") {
+      throw new Error("[ERROR] 선택할 수 없는 기능입니다.");
     }
   },
 };
