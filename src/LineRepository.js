f4177e2451c30bef8d1cf7295ef711d9ac0ec7fc
woo-jk <201702042@o.cnu.ac.kr>
2022-12-16 19:57:07 +0900

    feat: LineRepository 클래스 생성

---
+++ src/LineRepository.js
@@ -0,0 +1,15 @@
+class LineRepository {
+  #lines = [];
+
+  lines() {
+    return this.#lines;
+  }
+
+  addLine(line) {
+    this.#lines.push(line);
+  }
+
+  deleteStation(name) {
+    this.#lines = this.#lines.filter((line) => line.getName() !== name);
+  }
+}
