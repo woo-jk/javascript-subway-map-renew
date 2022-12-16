7ad2ba472fff1af2860c0a6d0fa2580f5db8bf07
woo-jk <201702042@o.cnu.ac.kr>
2022-12-16 20:23:16 +0900

    feat: OutputView 노선 관련 출력 함수들 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,12 +4,27 @@ const OutputView = {
   printRegisterStation() {
     MissionUtils.Console.print("\n[INFO] 지하철 역이 등록되었습니다.");
   },
+
   printdeleteStation() {
     MissionUtils.Console.print("\n[INFO] 지하철 역이 삭제되었습니다.");
   },
+
   printStations(stations) {
     MissionUtils.Console.print("\n## 역 목록");
     stations.forEach((station) => MissionUtils.Console.print(`[INFO] ${station.getName()}`));
+  },
+
+  printRegisterLine() {
+    MissionUtils.Console.print("\n[INFO] 지하철 노선이 등록되었습니다.");
+  },
+
+  printdeleteLine() {
+    MissionUtils.Console.print("\n[INFO] 지하철 노선이 삭제되었습니다.");
+  },
+
+  printLines(lines) {
+    MissionUtils.Console.print("\n## 노선 목록");
+    lines.forEach((line) => MissionUtils.Console.print(`[INFO] ${line.getName()}`));
   },
 };
 
