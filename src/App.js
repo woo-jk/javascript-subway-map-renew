5922c0c7fb58022a36e24b48b59f17b0ce8cec4d
woo-jk <201702042@o.cnu.ac.kr>
2022-12-16 22:32:37 +0900

    feat: App 초기 노선, 역 설정 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 const Line = require("./domain/Line");
+const LineRepository = require("./domain/LineRepository");
 const Station = require("./domain/Station");
+const StationRepository = require("./domain/StationRepository");
 const Validator = require("./Validator");
 const InputView = require("./view/InputView");
 const OutputView = require("./view/OutputView");
+
+const initStations = ["교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"];
+
+const initLines = [
+  new Line("2호선", "교대역", "역삼역"),
+  new Line("3호선", "교대역", "매봉역"),
+  new Line("신분당선", "강남역", "양재시민의숲역"),
+];
 
 class App {
   #lineRepository;
@@ -11,14 +21,26 @@ class App {
   constructor() {
     this.#lineRepository = new LineRepository();
     this.#stationRepository = new StationRepository();
+    this.initSetting();
+  }
+
+  #initStation() {
+    initStations.forEach((name) => this.#stationRepository.addStation(new Station(name)));
+  }
+
+  #initLine() {
+    initLines[0].addRoute("강남역", 2);
+    initLines[1].addRoute("남부터미널역", 2);
+    initLines[1].addRoute("양재역", 3);
+    initLines[2].addRoute("양재역", 2);
+
+    initLines.forEach((line) => this.#lineRepository.addLine(line));
   }
 
   initSetting() {
-    ["2호선", "3호선", "신분당선"].forEach((name) => this.#lineRepository.addLine(new Line(name)));
-
-    ["교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"].forEach((name) =>
-      this.#stationRepository.addStation(new Station(anme))
-    );
+    this.#initStation();
+    this.#initLine();
+    this.#lineRepository.getLines().forEach((line) => console.log(line.getRoute()));
   }
 
   play() {
